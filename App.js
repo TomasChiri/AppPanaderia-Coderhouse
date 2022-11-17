2f83e55bbb9c85165ca044173911cded207245eb
Tomas Chirino <tomasch2000@hotmail.com>
2022-11-16 23:13:49 -0300

    Clase 9: Pantallas y Navegacion Base

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import ShopNavigator from './src/navigation/ShopNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    "Tillana": require("./src/assets/fonts/Tillana-SemiBold.ttf")
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+
+  return <ShopNavigator />
 }
 
 const styles = StyleSheet.create({
